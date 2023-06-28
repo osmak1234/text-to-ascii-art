4bdff5e207a996d351e50b015333c2aba447efb9
osmak1234 <tadeashanus0631@gmail.com>
2023-06-28 21:35:26 +0200

    monospace, code cleanup
    cleared up the main.rs and moved into lib.rs
    modified ascii.rs to more  monospace characters,
    updated readme

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 ## Usage
 
 I made this crate for my project, if you need larger text in terminal, you need to use
-ascii art characters. This simple crate provides one function to make a ascii art string.
+ascii art characters. This simple crate provides function to make a ascii art string from text and to join 2 ascii art strings (they both need to be rectangle for it to work).
 
 fn convert (input: String) -> Result<String, String>
+
+If you cargo run this crate you can enter input text and it will just print and than exit.
 
 ## <a href="#-usage-in-rust"><img src="https://rustacean.net/assets/rustacean-flat-noshadow.svg" width="16" height="16"></a> Usage in Rust
 
@@ -23,4 +25,9 @@ fn main () {
 text-to-ascii-art="0.1"
 ```
 
-**Examples:** [`gymnasium/examples/`](gymnasium/examples/)
+## TODO
+
+- [x] kindof monospace
+- [ ] full monospace
+- [ ] add some more characters
+- [ ] eliminate the need for spacing
--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,19 @@
 pub mod ascii;
-use ascii::CHARS;
+use std::io;
+use text_to_ascii_art::convert;
 
-// You need to specialy concat ascii art because of how line brakes work, otherwise you would have just
-// everything vertically
-pub fn join_art(s1: &str, s2: &str) -> String {
-    // handles edge cases, with simple match
-    match (s1.is_empty(), s2.is_empty()) {
-        (true, true) => "".to_string(),
-        (true, false) => s2.to_string(),
-        (false, true) => s1.to_string(),
-        (false, false) => {
-            // case when both string are something
-            // each line has to have the same amount of characters, or other art will be shifted
-            // TODO: handle each line with different ammount of characters
+fn main() {
+    let mut input = String::new();
 
-            // you get all lines of the &str
-            let lines1: Vec<&str> = s1.split('\n').collect();
-            let lines2: Vec<&str> = s2.split('\n').collect();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
 
-            // concats each line of the 2 ascii art
-            let s3: Vec<String> = lines1
-                .into_iter()
-                .zip(lines2.into_iter())
-                .map(|(str1, str2)| str1.to_owned() + " " + str2)
-                .collect();
-
-            s3.join("\n")
+    for i in 0..100 {
+        input = i.to_string();
+        match convert(input) {
+            Ok(string) => println!("{}", string),
+            Err(err) => println!("Error: {}", err),
         }
     }
 }
-
-pub fn convert(input: String) -> Result<String, String> {
-    // substitutes everything with the equivalent in ascii art, or an empty string instead
-    let art_vector = input
-        .chars()
-        .map(|ch| CHARS.get(ch as usize).unwrap_or(&"").to_owned())
-        .collect::<Vec<&str>>();
-
-    // function to go through all the entered characters
-    let mut final_string = "".to_string();
-    let mut bad_char = false;
-    for art in &art_vector {
-        if art.is_empty() && !bad_char {
-            bad_char = true
-        }
-    }
-    // happens if you pass unhadled characters
-    if bad_char {
-        Err("Error: Some unallowed characters, you can use: a..=Z, 0..=9 ,`; : . , < > ( ) ! * # @ ^`".to_string())
-    } else {
-        for art in art_vector {
-            final_string = join_art(&final_string, art);
-        }
-        Ok(final_string)
-    }
-}
-fn main() {}
